0b1e639297039dbd11d00175c768ff05ec105f6d
rizqiwahyudi <rizqiwahyudi57@gmail.com>
2021-11-14 10:30:06 +0700

    Add some migrations, Edit AppServiceProvider, and Update dependencies

--- database/migrations/2021_11_12_033235_create_students_table.php
+++ database/migrations/2021_11_12_033235_create_students_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateStudentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('students', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('user_id')->constrained();
+            $table->string('name', 100);
+            $table->string('nrp', 20);
+            $table->string('class', 20);
             $table->timestamps();
             $table->softDeletes();
             $table->string('created_by')->nullable();
@@ -35,6 +34,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('students');
     }
 }
