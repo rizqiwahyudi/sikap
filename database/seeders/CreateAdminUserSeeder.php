729a4be6702956f00bf5bf0b3bbbdd57e2cd2d1b
rizqiwahyudi <rizqiwahyudi57@gmail.com>
2021-11-16 01:18:21 +0700

    Edit migrations, Edit kernel.php, & Add new seeders

---
+++ database/seeders/CreateAdminUserSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class CreateAdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::create([
+            'username'  => 'admin',
+            'email'     => 'admin@gmail.com',
+            'password'  => bcrypt('admin12345'),
+        ]);
+
+        $role = Role::create(['name' => 'admin']);
+
+        $permissions = Permission::pluck('id','id')->all();
+
+        $role->syncPermissions($permissions);
+
+        $user->assignRole([$role->id]);
+    }
+}
