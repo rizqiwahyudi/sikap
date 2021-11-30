e68b7b541765c9bd875d0a0ad08c5e21ab355c56
MNasirulUmam <nasirul250400@gmail.com>
2021-12-01 00:12:26 +0700

    edit view and add folder view

--- resources/views/layouts/apps.blade.php
+++ resources/views/layouts/apps.blade.php
@@ -37,11 +37,11 @@
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="">
                 <div class="sidebar-brand-icon rotate-n-15">
-                    <i class="fas fa-laugh-wink"></i>
+                    <i class="fas fa-fw fa-home"></i>
                 </div>
-                <div class="sidebar-brand-text mx-3">My Task</div>
+                <div class="sidebar-brand-text mx-3">SIKAP-PENS</div>
             </a>
 
             <!-- Divider -->
@@ -49,7 +49,7 @@
 
             <!-- Nav Item - Dashboard -->
             <li class="nav-item active">
-                <a class="nav-link" href="/admin">
+                <a class="nav-link" href="">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Dashboard</span></a>
             </li>
@@ -64,25 +64,50 @@
 
             <!-- Nav Item - Utilities Collapse Menu -->
             <li class="nav-item active">
-                <a class="nav-link" href="">
-                    <i class="fas fa-store-alt"></i>
-                    <span>Company</span></a>
-              
+                <a class="nav-link" href="" data-toggle="collapse" data-target="#collapseTwo"
+                    aria-expanded="true" aria-controls="collapseTwo">
+                    <i class="fas fa-building"></i>
+                    <span>Kerja Praktek</span>
+                </a>
+                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header">Kerja Praktek:</h6>
+                        <a class="collapse-item" href="">List KP</a>
+                        <a class="collapse-item" href="">Add KP</a>
+                    </div>
+                </div>
             </li>
 
             <!-- Nav Item - Pages Collapse Menu -->
             <li class="nav-item active">
-                <a class="nav-link" href="">
-                    <i class="far fa-building"></i>
-                    <span>Departement</span></a>
-              
+                <a class="nav-link" href="" data-toggle="collapse" data-target="#collapseUtilities"
+                    aria-expanded="true" aria-controls="collapseUtilities">
+                    <i class="fas fa-store-alt"></i>
+                    <span>Result</span>
+                </a>
+                <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
+                    data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header">Result Kerja Praktek:</h6>
+                        <a class="collapse-item" href="">All Result</a>
+                        <a class="collapse-item" href="">Add Result</a>
+                    </div>
+                </div>
             </li>
 
             <li class="nav-item active">
-                <a class="nav-link" href="">
+                <a class="nav-link" href=""data-toggle="collapse" data-target="#collapsePages"
+                    aria-expanded="true" aria-controls="collapsePages">
                     <i class="fas fa-users"></i>
-                    <span>User</span></a>
-              
+                    <span>User</span>
+                </a>
+                <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header">Users:</h6>
+                        <a class="collapse-item" href="">Student</a>
+                        <a class="collapse-item" href="">Teacher</a>
+                    </div>
+                </div>
             </li>
 
             <!-- Divider -->
