c28060f485924674bb32816acae2153688847629
rizqiwahyudi <rizqiwahyudi57@gmail.com>
2021-12-02 14:57:55 +0700

    Edit HomeController, Create loginAndResetPassword layout, and Edit login.blade.php, Email.blade.php, and also Update Dependencies

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,30 +1,7 @@
+@extends('layouts.loginAndResetPassword')
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>@yield('title')</title>
-
-    <!-- Custom fonts for this template-->
-    <link href="{{ asset('sb-admin2/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
-
-    <!-- Custom styles for this template-->
-    <link href="{{ asset('css/sb-admin-2.min.css')}}" rel="stylesheet">
-
-</head>
-
-<body class="bg-gradient-primary">
-
+@section('title', 'Login')
+@section('content')
     <div class="container">
 
         <!-- Outer Row -->
@@ -93,7 +70,7 @@
                                             </div>
                                         </div>
                                     </form> 
-                                   
+                                
                                     <hr>
                                     <div class="text-center">
                                         @if (Route::has('password.request'))
@@ -113,16 +90,4 @@
         </div>
 
     </div>
-
-</body>
-
-</html>
-<!-- Bootstrap core JavaScript-->
-<script src="{{ asset('sb-admin2/jquery/jquery.min.js')}}"></script>
-<script src="{{ asset('sb-admin2/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-
-<!-- Core plugin JavaScript-->
-<script src="{{ asset('sb-admin2/jquery-easing/jquery.easing.min.js')}}"></script>
-
-<!-- Custom scripts for all pages-->
-<script src="{{ asset('sb-admin2/js/sb-admin-2.min.js')}}"></script>
+@endsection
